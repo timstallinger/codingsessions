70998fd3b85085c1f802d2975c7fe43b8b4a15fe
Jonathan Pitzen <jopitzen@students.uni-mainz.de>
2022-11-09 11:07:43 +0100

    small changes to weapons(mostly comments)

--- Session1/weapons.py
+++ Session1/weapons.py
@@ -2,36 +2,46 @@ import pygame
 import math
 import random
 
-
+BLACK = ( 0, 0, 0)
 #weapons class for player to use
 
-class weapon(pygame.sprite.Sprite,...,...,...):
-    def __init__(self, x, y, color, name,ammo,damage,range,rate_of_fire,accuracy):
+class weapon(pygame.sprite.Sprite):
+    def __init__(self, x, y, name,ammo,damage,range,rate_of_fire,accuracy):
         super().__init__()
-        self.image = pygame.Surface([20, 20])
-        self.image.fill(color)
+        self.image = pygame.Surface([10, 20])
+        self.image.fill(BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.name = name
         self.ammo = ammo
         self.damage = damage
+        self.range = range
+        self.rate_of_fire = rate_of_fire
+        self.accuracy = accuracy
 
 
+    def spawn_weapon(all_sprites_list):
+    
+        weapon= (100,150 , BLACK, 6, 12, 15,1,1, 0.9)
+        all_sprites_list.add(weapon)
+        print("hola")
+        return weapon
+        
     def update(self):
         pass
 
-    def pickup(self):
+    def pickup_n_drop(self):
         pass
 
-    def use(self):
+    
+    def reload(self):
         pass
 
-    def drop(self):
-        pass
-
-    def get_name(self):
-        return self.name
+    def get_ammo(self):
+        return self.ammo
 
     def get_hitbox(self):
         return self.hitbox
+
+    #TODO: add shot parameters and link up with bullet class to pass projectiles
