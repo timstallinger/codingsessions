7f7bf4d6d10b13bf28f412ca192c0e274f9f3b87
Alshawty <117284651+Alshawty@users.noreply.github.com>
2022-11-20 12:47:11 +0100

    Update level_creation.py
    added variable wall amounts

--- Session1/level_creation.py
+++ Session1/level_creation.py
@@ -4,10 +4,11 @@ class wall_location:
     def __init__(self, number):
         super().__init__()
         self.number = number
-        self.walls = [[(200, 100, 50, 800),(400, 0, 50, 400),(400, 450, 50, 200)], [(400, 450, 50, 800),(100, 0, 50, 300),(0,0,0,0)]]
+        self.walls = [[(200, 100, 50, 800),(400, 0, 50, 400),(400, 450, 50, 200)], [(400, 450, 50, 800),(100, 0, 50, 300),(0,0,0,0)],[(150, 100, 50, 100),(250, 200, 100, 300),(400, 250, 50, 200),(400, 450, 250, 900)]]
+        self.walls = self.walls[number]
         
     def build_walls(self, i):
-        return self.walls[self.number][i]
+        return self.walls[i]
 
 
 def create_room():
@@ -44,8 +45,8 @@ def create_room():
     return wall_list
 
 def create_objects(wall_list, player, enemy):
-    room_nr = random.randint(0,1)
-    wall_list = room(wall_list, room_nr)
+    room_nr = random.randint(0,2)
+    wall_list = room(wall_list, 2)
     # if wall touches player or enemy, move it
 
     return 0
@@ -53,7 +54,9 @@ def create_objects(wall_list, player, enemy):
 def room(wall_list, number):
     #add variable iterator
     wall_loc = wall_location(number)
-    for i in range (3):
+    
+    for i in range (len(wall_loc.walls)):
+
         wall = pygame.sprite.Sprite()
         loc = wall_loc.build_walls(i)
         wall.image = pygame.Surface([loc[2],loc[3]])
